6272d74e5dfcc7a8d651c7414109b316ce2a65ad
madomado <madonuko@outlook.com>
2023-09-18 23:42:56 +0800

    feat: docs for cfg

--- src/cfg.rs
+++ src/cfg.rs
@@ -5,26 +5,44 @@ use smartstring::alias::String as SStr;
 
 #[derive(Deserialize, Debug)]
 pub struct Config {
-	pub distro: String, // "Ultramarine-Linux"
+	/// The name of the distro / edition.
+	/// This is used to name the directory with the ISO content.
+	pub distro: String,
+	/// The path to the root of the installation.
+	/// The directory will be created if it does not exist.
+	///
+	/// Otherwise, the content inside will remain, which may be
+	/// useful for some configuration files that DNF will not overwrite.
 	pub instroot: PathBuf,
+	/// The path to the ISO file to be created.
 	pub out: String,
+	/// Packages to be installed onto the new system.
 	pub packages: Vec<SStr>,
+	/// Configuration for the new system.
 	pub sys: System,
+	/// Scripts to be run during setup.
 	pub script: Script,
+	/// The command used for dnf. By default this is "dnf5".
 	pub dnf: Option<String>,
+	/// The volume id of the ISO file.
 	pub volid: String,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct System {
-	pub releasever: u8, // 38
+	/// The release version of the new system.
+	pub releasever: u8,
+	/// The root password of the new system.
 	pub rootpw: SStr,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct Script {
+	/// The path to the init script.
+	/// The init script is run after the mountpoint is created but before the packages are installed.
 	pub init: Option<PathBuf>,
+	/// The path to the pre-installation script.
+	/// The pre-installation script is run after package installations, `dracut` and root password setup.
+	/// `squashfs` will be run after this.
 	pub postinst: Option<PathBuf>,
 }
-
-// we copy stuff from instroot to isodir
