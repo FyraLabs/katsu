2af918538862da0a567af0609660c77fde59f887
Cappy Ishihara <cappy@cappuchino.xyz>
2023-09-19 04:07:11 +0700

    patch: complementary sysusers

--- tests/postinst.sh
+++ tests/postinst.sh
@@ -4,11 +4,14 @@ systemctl disable systemd-networkd-wait-online systemd-networkd systemd-networkd
 systemctl disable chronyd
 # systemctl disable polkit
 # Add polkitd user manually, weird hack because preinstall script doesn't run
-cat << EOF > /usr/lib/sysusers.d/polkitd.conf
-# This is a really weird hack because polkitd should've already been added by the preinstall script
+cat << EOF > /usr/lib/sysusers.d/extras.conf
+# This is a really weird hack
 #Type  Name    ID   Argument
 g      polkitd 114  
 u      polkitd 114:114 "User for polkitd" - -
+
+g      rpc     32
+u      rpc     32:32  "Rpcbind Daemon" - -
 EOF
 
 echo max_parallel_downloads=20 >> /etc/dnf/dnf.conf
