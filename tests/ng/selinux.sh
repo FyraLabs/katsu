913e8aa464407799785ad65aa3ae483195af391a
Cappy Ishihara <cappy@cappuchino.xyz>
2023-10-02 04:11:30 +0700

    fix selinux script

--- tests/ng/selinux.sh
+++ tests/ng/selinux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 
 setfiles -v -F -e /proc -e /sys -e /dev -e /bin /etc/selinux/targeted/contexts/files/file_contexts / || true
-setfiles -v -F -e /proc -e /sys -e /dev -e /etc/selinux/targeted/contexts/files/file_contexts.bin /bin || true
+setfiles -v -F -e /proc -e /sys -e /dev /etc/selinux/targeted/contexts/files/file_contexts.bin /bin || true
