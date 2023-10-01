b8e5fbab0a8b5ab29cc681a3c065f5b7343c81bc
Cappy Ishihara <cappy@cappuchino.xyz>
2023-10-02 04:00:51 +0700

    selinux: ignore errors

--- tests/ng/selinux.sh
+++ tests/ng/selinux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 
-setfiles -v -F -e /proc -e /sys -e /dev -e /bin /etc/selinux/targeted/contexts/files/file_contexts /
-setfiles -v -F -e /proc -e /sys -e /dev -e /etc/selinux/targeted/contexts/files/file_contexts.bin /bin
+setfiles -v -F -e /proc -e /sys -e /dev -e /bin /etc/selinux/targeted/contexts/files/file_contexts / || true
+setfiles -v -F -e /proc -e /sys -e /dev -e /etc/selinux/targeted/contexts/files/file_contexts.bin /bin || true
