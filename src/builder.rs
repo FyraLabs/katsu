d6c5b9c3cc8606b7f5408b75503e29e409a5713f
Cappy Ishihara <cappy@cappuchino.xyz>
2023-11-21 18:48:43 +0700

    Fix argument order in dr_args vector

--- src/builder.rs
+++ src/builder.rs
@@ -546,7 +546,7 @@ impl IsoBuilder {
 		// combine them all into one string
 
 		let dr_args2 =
-			vec!["--nomdadmconf", "--nolvmconf", "-vfN", "-a", &dr_mods, "-o", &dr_extra_args];
+			vec!["--nomdadmconf", "--nolvmconf", "-vfN", "-a", &dr_mods, &dr_extra_args];
 		let mut dr_args = vec![];
 
 		dr_args.extend(dr_basic_args);
